5500845272e35364ec2f44e1ee58aa7eea0a7506
obmoras <olga.mora.mx@ieee.org>
2020-11-16 14:30:00 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,11 @@ module.exports = {
   siteMetadata: {
     title: `Creative Portfolio`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
-    {
+//  plugins: [
+//   `gatsby-plugin-react-helmet`,
+//    `gatsby-plugin-sass`,
+//    `gatsby-transformer-remark`,
+//    {
 //      resolve: `gatsby-source-datocms`,
 //      options: {
 //        apiToken: process.env.DATO_API_TOKEN,
